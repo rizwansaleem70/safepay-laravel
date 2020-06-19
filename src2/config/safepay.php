3fe7fc70ff0ccd1ecc247da6d4478d76bde2358c
Rizwan Saleem <rizwansaleem70@gmail.com>
2020-06-19 23:44:17 +0500

    added src folder

---
+++ src2/config/safepay.php
@@ -0,0 +1,12 @@
+<?php 
+
+
+return [
+    "environment"  => "sandbox", //use 'production' for live payments
+    "api_key" => "",
+    'redirect_url' => "",
+    'cancel_url' => "",
+    'currency' => "",
+    'webhook_secret_key' => "",
+    'order_class' => "" // Order Class Here
+];
